e6aa3aaa0f0f27d3a7da3dc3effa5b03a710f1a5
SoleEsp <soledad.espiritu909@gmail.com>
2023-02-17 15:04:11 -0300

    first

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 from fastapi import FastAPI
 import pandas as pd
-from my_module import get_max_duration
+from query import *
 
 app = FastAPI()
+
+@app.on_event('startup')
+def startup():
+    global df_netflix_ETL, df_amazon_prime_ETL, df_hulu_ETL, df_netflix_ETL, df_disney_plus_ETL
+    df_amazon_prime_ETL = pd.read_csv('./dataset_ETL/amazon_prime_ETL.csv')
+    df_disney_plus_ETL = pd.read_csv('./dataset_ETL/disney_plus_ETL.csv')
+    df_hulu_ETL = pd.read_csv('./dataset_ETL/hulu_ETL.csv')
+    df_netflix_ETL = pd.read_csv('./dataset_ETL/netflix_ETL.csv')
 
 @app.get("/saludar")
 def saludar():
     return {"mensaje": "Hola mundo"}
 
-@app.get("/get_max_duration")
-def get_max_duration(year: int, platform: str, duration_type: str):
-    # Llama a la función get_max_duration con los parámetros recibidos
-    result = get_max_duration(year, platform, duration_type)
-    return result
+@app.get("/get_max_duration/({anio},{plataforma},{min_o_season})")
+async def get_max_duration_1(year: int, platform: str, duration_type: str):
+    max_duration_movie = get_max_duration(year, platform, duration_type)
+    return {"Titulo": max_duration_movie}
+
+@app.get('/get_count_platform/({plataforma})')
+async def get_count_platform_2(platform: str):
+    movie = get_count_platform(platform)
+    return {'Cantidad de peliculas por plataforma':movie}
+
+@app.get('/get_actor/({plataforma},{anio})')
+async def get_actor_3(platform: str, year: int):
+    most_frequent_actor = get_actor(platform, year)
+    return {'El actor que mas se repite': most_frequent_actor}
+    
--- query.py
+++ query.py
@@ -2,28 +2,61 @@ import pandas as pd
 
 #************************************ QUERYS**************************************************
 
+df_amazon_prime_ETL = pd.read_csv('./dataset_ETL/amazon_prime_ETL.csv')
+df_disney_plus_ETL = pd.read_csv('./dataset_ETL/disney_plus_ETL.csv')
+df_hulu_ETL = pd.read_csv('./dataset_ETL/hulu_ETL.csv')
+df_netflix_ETL = pd.read_csv('./dataset_ETL/netflix_ETL.csv')
+
 def get_max_duration(year, platform, duration_type):
     # diccionario de nombres de archivo CSV para cada plataforma
     dataframe = {
-        'Netflix': df_netflix,
-        'Amazon': df_amazon_prime,
-        'Disney': df_disney_plus,
-        'Hulu': df_hulu
+        'netflix': df_netflix_ETL,
+        'amazon': df_amazon_prime_ETL,
+        'disney': df_disney_plus_ETL,
+        'hulu': df_hulu_ETL
     }
 
-    if platform not in dataframe:
-        return print(f'La plataforma "{platform}" no es válida. Las plataformas válidas son: Netflix, Disney, Amazon, Hulu.')
+    selected_df = dataframe[platform]
+    
+    selected_movies = selected_df.loc[(selected_df["release_year"] == year) & (selected_df["duration_type"] == duration_type)]
+        
+    max_duration_movie = selected_movies.loc[selected_movies["duration_int"].idxmax()]['title']
+
+    return max_duration_movie
+
+def get_count_platform(platform):
+    
+    dataframe = {
+        'netflix': df_netflix_ETL,
+        'amazon': df_amazon_prime_ETL,
+        'disney': df_disney_plus_ETL,
+        'hulu': df_hulu_ETL
+    }
+    
+    selected_df = dataframe[platform]
+    movie = selected_df[selected_df['type'] == 'movie']
+    
+    return movie.shape[0]
+
+def get_actor(platform,year):
+    dataframe = {
+        'netflix': df_netflix_ETL,
+        'amazon': df_amazon_prime_ETL,
+        'disney': df_disney_plus_ETL,
+        'hulu': df_hulu_ETL
+    }
     
     selected_df = dataframe[platform]
     
-    if duration_type not in ['min', 'season']:
-        return print(f'Por favor ingrese alguno de los siguientes valores correctos\n duration_type: min o season')
+    df_year = selected_df[selected_df['release_year'] == year]
     
-    selected_movies = selected_df.loc[(selected_df["release_year"] == year) & (selected_df["duration_type"] == duration_type)]
+    actor_counts = df_year['cast'].str.split(',').explode().str.strip().value_counts()
+    
+    most_frequent_actor = actor_counts.index[0]
+    
+    return most_frequent_actor
 
-    if selected_movies.empty:
-        return print(f"No hay resultados para el año {year}. Intente con otro año.")
-            
-    max_duration_movie = selected_movies.loc[selected_movies["duration_int"].idxmax()]['title']
+get_actor('amazon',2020)
+# get_count_platform('amazon')
 
-    return print('Titulo: ',max_duration_movie)
+#get_max_duration(2020,'disney','min')
