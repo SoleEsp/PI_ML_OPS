d4b41dd5840e1ad871b209c16f2b42c02cd72d98
SoleEsp <soledad.espiritu909@gmail.com>
2023-02-20 22:53:18 -0300

    firs

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 from fastapi import FastAPI
 import pandas as pd
+#from surprise import Dataset, Reader, SVD
+#from surprise.model_selection import cross_validate
 from query import *
 
 app = FastAPI()
 
 @app.on_event('startup')
 def startup():
-    global df_netflix_ETL, df_amazon_prime_ETL, df_hulu_ETL, df_netflix_ETL, df_disney_plus_ETL
+    global df_netflix_ETL, df_amazon_prime_ETL, df_hulu_ETL, df_netflix_ETL, df_disney_plus_ETL, df_platforms_ETL
     df_amazon_prime_ETL = pd.read_csv('./dataset_ETL/amazon_prime_ETL.csv')
     df_disney_plus_ETL = pd.read_csv('./dataset_ETL/disney_plus_ETL.csv')
     df_hulu_ETL = pd.read_csv('./dataset_ETL/hulu_ETL.csv')
     df_netflix_ETL = pd.read_csv('./dataset_ETL/netflix_ETL.csv')
+    df_platforms_ETL =  pd.read_csv('./dataset_ETL/df_platforms_ETL.csv')
+    
 
 @app.get("/get_max_duration/({anio},{plataforma},{min_o_season})")
 async def get_max_duration_1(year: int, platform: str, duration_type: str):
     max_duration_movie = get_max_duration(year, platform, duration_type)
     return {"Titulo": max_duration_movie}
 
+@app.get("/get_scored_count/({plataforma},{puntaje},{anio})")
+async def get_scored_count_2(platform: str, scored: float, year: int):
+    count = get_score_count(platform, scored, year)
+    return {count}
+
 @app.get('/get_count_platform/({plataforma})')
-async def get_count_platform_2(platform: str):
+async def get_count_platform_3(platform: str):
     movie = get_count_platform(platform)
     return {'Cantidad de peliculas por plataforma':movie}
 
 @app.get('/get_actor/({plataforma},{anio})')
-async def get_actor_3(platform: str, year: int):
+async def get_actor_4(platform: str, year: int):
     most_frequent_actor = get_actor(platform, year)
     return { most_frequent_actor}
-    
+
+
+#@app.get('/recommend_movie/({idusuario},{idpelicula}')
+#async def recommend_movie_5(user_id: int, movie_id: str):
+#    recommend = recommend_movie(user_id, movie_id)
+#    return {recommend}
--- query.py
+++ query.py
@@ -1,4 +1,7 @@
 import pandas as pd
+#from surprise import Dataset, Reader, SVD
+#from surprise.model_selection import cross_validate
+
 
 #************************************ QUERYS**************************************************
 
@@ -7,8 +10,12 @@ df_disney_plus_ETL = pd.read_csv('./dataset_ETL/disney_plus_ETL.csv')
 df_hulu_ETL = pd.read_csv('./dataset_ETL/hulu_ETL.csv')
 df_netflix_ETL = pd.read_csv('./dataset_ETL/netflix_ETL.csv')
 
+df_platforms_ETL = pd.read_csv('./dataset_ETL/df_platforms_ETL.csv')
 
 # diccionario de nombres de archivo CSV para cada plataforma
+# se sigue manteniendo de esta forma para que la ejecucion de los query sea mas rapida
+# solo se utlizara el df_platforms cuando sea necesario, ya que la ejecucion demora en dar un resultado
+
 dataframe = {
         'Netflix': df_netflix_ETL,
         'Amazon': df_amazon_prime_ETL,
@@ -35,6 +42,30 @@ def get_max_duration(year, platform, duration_type):
 
     return max_duration_movie
 
+def get_score_count(platform, scored, year):
+    
+    #validacion de palabras correctas
+    if platform not in dataframe.keys():
+        return f'La plataforma "{platform}" no es válida. Las plataformas válidas son: Netflix, Disney, Amazon, Hulu.'
+    
+    # Obtener la inicial de la plataforma
+    platform_id = platform.lower()[0]
+    
+    df_filtered = df_platforms_ETL[df_platforms_ETL['type'] == 'movie']
+    
+    # Filtrar el DataFrame por plataforma y año
+    df_filtered = df_platforms_ETL[(df_platforms_ETL['id'].str.startswith(platform_id)) & (df_platforms_ETL['release_year'] == year)]
+    
+    if df_filtered.empty:
+        return f"No se encontraron resultados para el año {year}. Intente con otro año."
+    
+    #value_counts = df_filtered['scored'].value_counts()
+    
+    # Contar la cantidad de películas que tienen un puntaje mayor a scored
+    count = len(df_filtered[df_filtered['scored'] > scored])
+    
+    return f'Cantidad de peliculas : {count}'
+
 
 def get_count_platform(platform):
     
@@ -56,7 +87,6 @@ def get_actor(platform,year):
     df_year = selected_df[selected_df['release_year'] == year]
     
     if df_year.empty:
-        
         return f"No se encontraron resultados para el año {year}. Intente con otro año."
     
     actor_counts = df_year['cast'].str.split(',').explode().str.strip().value_counts()
@@ -64,3 +94,29 @@ def get_actor(platform,year):
     most_frequent_actor = actor_counts.index[0]
     
     return f"Actor: {most_frequent_actor}, Cantidad de veces repetida: {actor_counts[most_frequent_actor]}"
+
+''''
+def recommend_movie(user_id, movie_id):
+    # Seleccionar solo las columnas necesarias
+    ratings = df_platforms_ETL[['userId', 'title', 'scored']]
+
+    # Definir el rango de calificaciones
+    reader = Reader(rating_scale=(1, 5))
+
+    # Cargar los datos
+    data = Dataset.load_from_df(ratings, reader)
+
+    # Entrenar el modelo
+    algo = SVD()
+    trainset = data.build_full_trainset()
+    algo.fit(trainset)
+
+    # Predecir la calificación del usuario para la película
+    prediction = algo.predict(user_id, movie_id, verbose=False)
+
+    # Obtener la predicción y devolver si la película es recomendada o no
+    if prediction.est >= 3.5:
+        return f"La película es recomendada."
+    else:
+        return f"La película no es recomendada."
+'''
